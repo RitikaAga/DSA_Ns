49059cac45438d81d24cbf93f51fb9a499328bc3
Ritika Agarwal <106179540+RitikaAga@users.noreply.github.com>
2022-07-08 20:20:20 +0530

    Create insertion_sort.java

---
+++ sorting/insertion_sort.java
@@ -0,0 +1,7 @@
+//Insertion sort
+// in insertion short we 1st compare 1st element to 2nd element    7 5 1 6 2
+// if 1st elemnt is greater then we shift it to the right and 2nd elemnt take 1st element's place  5 7 1 6 2
+// now we again check 3rd element 1st from 2nd then from 1
+// if it is greator then the 1st element then it will take place of 1st element and 1st will shift to the 2nd index .  1 5 7 6 2 
+// again it will check same this process would follow until the array will not short   1 2 5 6 7
+// Tc = O(n) in best case O(n^2) in average and worst case 
