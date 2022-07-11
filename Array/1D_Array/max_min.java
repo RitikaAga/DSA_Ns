62164b5b5a141f9da27b66850d6e74d65fb011c7
Ritika Agarwal <106179540+RitikaAga@users.noreply.github.com>
2022-07-11 14:43:59 +0530

    Update and rename max.java to max_min.java

--- Array/1D_Array/max_min.java
+++ Array/1D_Array/max_min.java
@@ -3,15 +3,21 @@ public class Main {
     public static void main(String args[]) {
       Scanner sc = new Scanner(System.in);
       int n = sc.nextInt();
-      int a[] = new int[n];
-     int max = sc.nextInt();
-      for(int i =0;i<n;i++){
+      int a[] = new int[n]; 
+       for(int i =0;i<n;i++){
               a[i] = sc.nextInt();
      }
+      int max = a[0];
       for(int i =0;i<n;i++){
           if(a[i]>max){
               max = a[i];
               }}
           System.out.println(max);
+          int min = a[0];
+          for(int i =0;i<n;i++){
+         if(a[i]<min){
+              min = a[i];
+              }}
+              System.out.println(min);
     }
 }
