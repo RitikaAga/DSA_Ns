f986842ddd60c2893713656221d1ad29d4c2ae22
Ritika Agarwal <106179540+RitikaAga@users.noreply.github.com>
2022-07-12 19:32:43 +0530

    Create basic2.java

---
+++ string/basic2.java
@@ -0,0 +1,25 @@
+//  Strings are immutable we can not modity or change it 
+//if we want to modify it so we have to create a new string 
+
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+     StringBuilder sb = new StringBuilder("Tony");
+     System.out.println(sb);
+
+     //char at index 0
+     System.out.println(sb.charAt(0));
+
+//set char at index 0
+sb.setCharAt(0,'M');
+System.out.println(sb);
+
+//insert character at some position
+sb.insert(3,'y');
+System.out.println(sb);
+
+//delete character at some position
+sb.delete(3,4);
+System.out.println(sb);
+    }
+}
