9455455df8bd5ac5962dd4687fe0a4709aa104d8
Ritika Agarwal <106179540+RitikaAga@users.noreply.github.com>
2022-07-12 17:19:17 +0530

    Create peak.java

---
+++ Binary search/peak.java
@@ -0,0 +1,21 @@
+/find peak;
+
+
+class Solution {
+    public int findPeakElement(int[] ar) {
+       int start=0,end = ar.length-1;
+        while(start<end){
+        int mid = (start + (end-start)/2);
+            if(ar[mid]> ar[mid+1]){
+              end = mid;
+            }
+
+            else {
+            start = mid + 1;
+            }
+
+
+    }
+         return end;
+}
+}
