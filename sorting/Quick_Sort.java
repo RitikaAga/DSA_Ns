a13318f0c8bab71915b66441fb443d1e80b84b21
Ritika Agarwal <106179540+RitikaAga@users.noreply.github.com>
2022-07-20 20:47:24 +0530

    Create Quick_Sort.java

---
+++ sorting/Quick_Sort.java
@@ -0,0 +1,40 @@
+public class Main {
+    public static int partition(int arr[], int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        i++;
+        int temp = arr[i];
+        arr[i] = pivot;
+        arr[high] = temp;
+        return i;
+    }
+
+    public static void quickSort(int arr[], int low, int high) {
+        if (low < high) {
+            int pidx = partition(arr, low, high);
+
+            quickSort(arr, low, pidx - 1);
+            quickSort(arr, pidx + 1, high);
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 5, 2, 9, 4, 7, 6, 3 };
+
+        quickSort(arr, 0, arr.length - 1);
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
