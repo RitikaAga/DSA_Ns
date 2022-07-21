ea0aeaaaee8faa9085e37ef1559e86efb3f0c9e3
Ritika Agarwal <106179540+RitikaAga@users.noreply.github.com>
2022-07-21 17:45:22 +0530

    Update Every_no_freqq.java

--- hashing/Every_no_freqq.java
+++ hashing/Every_no_freqq.java
@@ -26,3 +26,67 @@ class Main {
 	        }
 	            }
 	            }
+
+
+// 2 approach
+
+import java.util.*;
+public class Main {
+    public static void main(String args[]) {
+      Scanner sc = new Scanner(System.in);
+      int n = sc.nextInt();
+      int a[] = new int[n];
+      int b[] = new int[n];
+    int count = 0;
+      for(int i = 0; i<n;i++){
+          a[i] = sc.nextInt();
+      }
+      for(int i= 0; i<n;i++){
+                count = 1;
+                if(a[i]!=-1){
+                for(int j= i+1; j<n;j++){
+             if(a[i]==a[j]){
+              count++ ;
+              a[j] = -1;
+              }
+      }
+      b[i] = count;
+      }
+      }
+       for(int i= 0; i<n;i++){
+       if(a[i]!=-1){
+        System.out.println(a[i] + " " + b[i]);
+        }
+        }
+        }
+        }
+
+
+// 3rd APPROACH
+
+import java.util.*;
+public class Main
+{
+public static void main(String[] args) {
+ Scanner sc = new Scanner(System.in);
+      int n = sc.nextInt();
+      int a[] = new int[n];
+      for(int i = 0; i<n;i++){
+          a[i] = sc.nextInt();
+      }
+frequency(a);
+}
+static void frequency(int []a){
+int n = a.length;
+int[] freq = new int[10];
+for(int i=0; i<n;i++){
+freq[a[i]]++;
+}
+ for(int i=0;i<n;i++){ // O(n) time complexity
+ if(freq[a[i]]!=0){
+ System.out.println(a[i] + " " + " " + freq[a[i]] + " ");
+freq[a[i]] = 0;
+}
+}
+}
+}
