4d08a31f7ecb35dfbc2331a458208427ece52679
Ritika Agarwal <106179540+RitikaAga@users.noreply.github.com>
2022-07-21 16:47:38 +0530

    Update PrimeNo.java

--- Math/PrimeNo.java
+++ Math/PrimeNo.java
@@ -4,14 +4,17 @@ public class Main
 	public static void main(String[] args) {
 	    Scanner sc = new Scanner(System.in);
 	    int n   = sc.nextInt();
+	    boolean flag = true;
 	    for(int i = 2; i<=n/2; i++){
-	   if(n%i != 0)
+	   if(n%i == 0){
+	       flag = false;
+	   }}
+	   if(flag == true)
 	   System.out.println(" Number is Prime");
 	   else{
 	    	   System.out.println("Number is not Prime");
     
-	   }
-	   break;
 	    }
 	}
 }
+
