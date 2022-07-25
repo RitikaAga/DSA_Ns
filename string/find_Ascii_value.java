da2de87594459c83595ce3e0ee63b6c6e77ff912
Ritika Agarwal <106179540+RitikaAga@users.noreply.github.com>
2022-07-25 17:41:34 +0530

    Update find_Ascii_value.java

--- string/find_Ascii_value.java
+++ string/find_Ascii_value.java
@@ -6,3 +6,14 @@ public class MyClass {
        System.out.println(ch1);    //32
     }
 }
+
+// find char from ascii value 
+
+public class MyClass {
+    public static void main(String args[]) {
+      int a = 65;
+      char ch = (char)a;
+      System.out.println(ch);   //A
+      
+    }
+}
