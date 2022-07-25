8020135612bc9253edf51c57ef142bd2fadf8fd8
Ritika Agarwal <106179540+RitikaAga@users.noreply.github.com>
2022-07-25 17:52:38 +0530

    Update lower_upper.java

--- string/lower_upper.java
+++ string/lower_upper.java
@@ -1,8 +1,10 @@
 public class MyClass {
     public static void main(String args[]) {
-      int a = 65;
-      char ch = (char)a;
-      System.out.println(ch);   //A
+     String s1 = "NeWtOn";
+     String s2 = s1.toLowerCase();   // will convert string into lower case 
+      System.out.println(s2);
       
+      String s3 = s1.toUpperCase();     // will convert string into upper case
+     System.out.println(s3); 
     }
 }
