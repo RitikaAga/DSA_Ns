99ea486cb07ae3fe8669f5834ed513c651a05936
Ritika Agarwal <106179540+RitikaAga@users.noreply.github.com>
2022-07-25 17:37:02 +0530

    Create char_At_index.java

---
+++ string/char_At_index.java
@@ -0,0 +1,12 @@
+public class MyClass {
+    public static void main(String args[]) {
+     String s1 = "Newton";
+     int n = s1.length();          // to find the length of string 
+     for( int i = 0;i<n;i++)
+     { 
+         char ch = s1.charAt(i);    // gives the character at the given index 
+      System.out.println(ch);
+     } 
+      
+    }
+}
