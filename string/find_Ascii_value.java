32c6f7a12db176a33b0519b1e88dde9ea63d0d2e
Ritika Agarwal <106179540+RitikaAga@users.noreply.github.com>
2022-07-25 17:13:01 +0530

    Create find_Ascii_value.java

---
+++ string/find_Ascii_value.java
@@ -0,0 +1,8 @@
+public class MyClass {
+    public static void main(String args[]) {
+      int ch = 'b';
+      int ch1 = ' ';
+      System.out.println(ch);    //98
+       System.out.println(ch1);    //32
+    }
+}
