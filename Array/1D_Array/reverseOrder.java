8373b606a6ebf01dae6f6068af45dc0400af4067
Ritika Agarwal <106179540+RitikaAga@users.noreply.github.com>
2022-07-27 22:05:15 +0530

    Update reverseOrder.java

--- Array/1D_Array/reverseOrder.java
+++ Array/1D_Array/reverseOrder.java
@@ -13,3 +13,30 @@ public class Main {
       }
       }
       }
+
+// 2nd approarch
+// tc = O(N)
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[]  =new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+       int start = 0;
+        int end = n-1;
+        while(start<=end){
+            int temp = arr[end];
+            arr[end] = arr[start];
+            arr[start]  = temp;
+            start++;
+            end--;
+        }
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+
+    }
+}
