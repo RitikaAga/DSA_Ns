c0f265cd3bf63814e6051c0a73ae71dc71523656
Ritika Agarwal <106179540+RitikaAga@users.noreply.github.com>
2022-07-31 12:41:20 +0530

    Create Stack_implementation_using_LinkedList.java

---
+++ Stack/Stack_implementation_using_LinkedList.java
@@ -0,0 +1,30 @@
+
+//fuct only
+
+Node top = null;
+public void push(int x){
+//enter your code here
+    Node nd = new Node(x);
+    nd.next = top;
+    top = nd;
+}
+
+public void pop(){
+//enter your code here
+    if(top == null){
+        return;
+    }
+    else{
+        top = top.next;
+    }
+}
+
+public void top(){
+//enter your code here
+    if(top == null){
+        System.out.println(0);
+    }
+    else{
+        System.out.println(top.val);
+    }
+}
