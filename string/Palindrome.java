23598244dd920684fabd7566026a489c12d4214c
Ritika Agarwal <106179540+RitikaAga@users.noreply.github.com>
2022-08-02 17:29:47 +0530

    Update Palindrome.java

--- string/Palindrome.java
+++ string/Palindrome.java
@@ -10,3 +10,19 @@ if (s<e)
     return check_Palindrome(str, s+1, e-1);
 return true;
     }
+
+// 2nd approach
+
+public class StringPalindrome {
+   public static void main(String args[]) {
+      String myString = "anna";
+      StringBuffer buffer = new StringBuffer(myString);
+      buffer.reverse();
+      String data = buffer.toString();
+      if(myString.equals(data)){
+         System.out.println("Given String is palindrome");
+      } else {
+         System.out.println("Given String is not palindrome");
+      }
+   }
+}
