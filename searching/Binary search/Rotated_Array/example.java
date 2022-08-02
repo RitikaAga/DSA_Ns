f87611f2664aa4aca9cf0223d8f1859e40e258c0
Ritika Agarwal <106179540+RitikaAga@users.noreply.github.com>
2022-08-02 16:04:40 +0530

    Update example.java

--- searching/Binary search/Rotated_Array/example.java
+++ searching/Binary search/Rotated_Array/example.java
@@ -32,18 +32,7 @@ public class Main {
                 start = mid+1;
             }
         }
-        return -1;
-        
-        
-        // 2nd approach
-        
-        for(int i = 0;i<arr.length;i++){
-            if(arr[i] == target){
-                return i;
-            }
-              }
-         return -1; 
-    }}
+        return -1;  
     }
     static int binarySearch(int[] arr, int start, int end, int target){
         while(start<=end){
@@ -59,3 +48,58 @@ public class Main {
         return -1;
     }
 }
+
+
+ // 2nd approach
+        
+        for(int i = 0;i<arr.length;i++){
+            if(arr[i] == target){
+                return i;
+            }
+              }
+         return -1; 
+    }}
+
+
+//3rd approach
+
+
+public class MyClass{
+public static void main(String[] args) {
+      int []  arr = {4,5,6,7,0,1,2};
+      int target = 2;
+      int temp = find(arr, target);
+        System.out.println(temp);
+
+    }
+    public static int find(int [] arr, int target){
+        int lo = 0;
+        int hi = arr.length - 1;
+        while(lo<= hi){
+            int mid = lo + (hi - lo)/2;
+            if(arr[mid] == target){
+                return mid;
+            }
+            else if(arr[lo]<= arr[mid]){
+                // lo to mid is sorted
+                if(target >= arr[lo] && arr[mid] > target){
+                    hi = mid -1;
+                }else{
+                    lo = mid +1;
+                }
+
+            }
+            else if(arr[mid] <= arr[hi]){
+                // mid to high part is sorted
+                if(target> arr[mid] && arr[hi] >= target){
+                    lo = mid +1;
+                }
+                else{
+                    hi = mid - 1;
+                }
+
+            }
+        
+        }
+        return -1;
+    }}
