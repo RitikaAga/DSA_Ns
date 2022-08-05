113d69651a1d744a7a1388ed9b639ba45023cc88
Ritika Agarwal <106179540+RitikaAga@users.noreply.github.com>
2022-08-05 23:51:32 +0530

    Update rotate.java

--- Array/1D_Array/rotate.java
+++ Array/1D_Array/rotate.java
@@ -18,3 +18,36 @@ class Solution {
        }
     }
 }
+
+
+//  2nd approach
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int arr[] = { 1, 2, 3, 4, 5 };
+        int new_array[] = new int[arr.length];
+        int k = 2;
+        int rotating_point = k;
+
+        int i = 0;
+        while (rotating_point < arr.length) {
+            new_array[i] = arr[rotating_point];
+            i++;
+            rotating_point++;
+        }
+        rotating_point = 0;
+        while (rotating_point < k) {
+            new_array[i] = arr[rotating_point];
+            i++;
+            rotating_point++;
+        }
+        System.out.println("Rotating array is: ");
+        
+        for (int j = 0; j < new_array.length; j++) {
+            System.out.print(new_array[j] + " ");
+            ;
+        }
+    }
+}
