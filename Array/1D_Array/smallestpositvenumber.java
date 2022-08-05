ccb597fabdf6ed86e8c5e1799c84f116c0f64342
Ritika Agarwal <106179540+RitikaAga@users.noreply.github.com>
2022-08-05 23:48:13 +0530

    Update smallestpositvenumber.java

--- Array/1D_Array/smallestpositvenumber.java
+++ Array/1D_Array/smallestpositvenumber.java
@@ -13,3 +13,24 @@ public class smallestpositvenumber {
 
     }
 }
+
+//count smallest positive 2nd approach
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+       int arr[] = {2, 1, 3, 4, 8, -1, -10, 15};
+       
+        Arrays.sort(arr);
+        
+        for(int i=0; i<arr.length-1; i++){
+            if(arr[i] > 0){
+                if((arr[i+1] - arr[i]) != 1){
+                    System.out.print(arr[i]+1);
+                    break;
+                }
+            }
+        }
+    }
+}
