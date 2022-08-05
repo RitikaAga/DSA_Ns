c25cd4ed70a71b9e8248cd72586fa551e3c8af6b
Ritika Agarwal <106179540+RitikaAga@users.noreply.github.com>
2022-08-05 23:54:07 +0530

    Update rotate.java

--- Array/1D_Array/rotate.java
+++ Array/1D_Array/rotate.java
@@ -51,3 +51,46 @@ public class Main {
         }
     }
 }
+
+
+//3rd approach
+
+public class MyClass{
+public static void main(String[] args) {
+      int []  arr = {4,5,6,7,0,1,2};
+      int target = 2;
+      int temp = find(arr, target);
+        System.out.println(temp);
+
+    }
+    public static int find(int [] arr, int target){
+        int lo = 0;
+        int hi = arr.length - 1;
+        while(lo<= hi){
+            int mid = lo + (hi - lo)/2;
+            if(arr[mid] == target){
+                return mid;
+            }
+            else if(arr[lo]<= arr[mid]){
+                // lo to mid is sorted
+                if(target >= arr[lo] && arr[mid] > target){
+                    hi = mid -1;
+                }else{
+                    lo = mid +1;
+                }
+
+            }
+            else if(arr[mid] <= arr[hi]){
+                // mid to high part is sorted
+                if(target> arr[mid] && arr[hi] >= target){
+                    lo = mid +1;
+                }
+                else{
+                    hi = mid - 1;
+                }
+
+            }
+        
+        }
+        return -1;
+    }}
