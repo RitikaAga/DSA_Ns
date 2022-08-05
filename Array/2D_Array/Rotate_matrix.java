376ef99edcf5b52db28b81c7c0879fbd1279eb6e
Ritika Agarwal <106179540+RitikaAga@users.noreply.github.com>
2022-08-05 23:13:18 +0530

    Create Rotate_matrix.java

---
+++ Array/2D_Array/Rotate_matrix.java
@@ -0,0 +1,46 @@
+//Rotate matrix by 90°
+
+public class Main {
+    public static void display(int arr[][]) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[][] = new int[n][n];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        // transpose
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr[0].length; j++) {
+                int temp = arr[i][j];
+                arr[i][j] = arr[j][i];
+                arr[j][i] = temp;
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            int left = 0;
+            int right = arr[i].length - 1;
+
+            while (left < right) {
+                int temp = arr[i][left];
+                arr[i][left] = arr[i][right];
+                arr[i][right] = temp;
+                left++;
+                right--;
+            }
+        }
+        display(arr);
+    }
+}
