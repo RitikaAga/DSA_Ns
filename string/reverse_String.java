e669db202a6453d8e739b53da430a3ae39aad7d1
Ritika Agarwal <106179540+RitikaAga@users.noreply.github.com>
2022-08-16 13:20:41 +0530

    Update reverse_String.java

--- string/reverse_String.java
+++ string/reverse_String.java
@@ -9,3 +9,29 @@ public class Main {
         System.out.print(rev);
     }
 }
+
+
+//  2nd Approach
+
+
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+	 System.out.println("Enter Your Name & See Magic \n");
+	  Scanner sc  = new Scanner(System.in);
+	  String name = sc.nextLine();
+	  var rev = name.toCharArray();
+	  int l = rev.length;
+	  int n = l/2;
+	  for(int i=0;i<n;i++){
+	    char temp = rev[l-i-1];
+	    rev[l-i-1] = rev[i];
+	    rev[i] = temp;
+	  }
+	  for(char e:rev){
+	    System.out.print(e);
+	  }
+	}
+}
