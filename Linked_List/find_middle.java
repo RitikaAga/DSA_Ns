d2f137dc252db107aa4e6046a664a58f57bb15d3
Ritika Agarwal <106179540+RitikaAga@users.noreply.github.com>
2022-08-16 13:15:00 +0530

    Update find_middle.java

--- Linked_List/find_middle.java
+++ Linked_List/find_middle.java
@@ -11,3 +11,73 @@ class Solution {
         return p1;
     }
 }
+
+    // complete program of find middle element of link list
+
+public class MyClass {
+    static Node head;
+    class Node
+    {
+        int data;
+        Node next;
+        public Node(int data)
+        {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    void insert(int data)
+    {
+        Node N1 = new Node(data);
+       if(head==null){
+       
+       head = N1;
+       }
+       else{
+           Node temp = head;
+           while(temp.next != null)
+           {
+               temp = temp.next;
+               
+           }
+           temp.next = N1;
+       }
+       
+      }
+    void middle()
+    {
+        Node p1 =head;
+        Node p2 = head;
+        while(p2!=null && p2.next !=null)
+        {
+           p1 = p1.next;
+           p2 = p2.next.next;
+        }
+        System.out.println("Middle element is " + p1.data + " ");
+        
+    }
+    void print()
+    {
+        Node curr = head;
+        while(curr !=null)
+        {
+            System.out.print(curr.data + "--->" + " ");
+            curr = curr.next;
+        }
+        System.out.print(null + " ");
+    } 
+    public static void main(String args[]) {
+     MyClass ll = new MyClass();
+     ll.insert(1);
+     ll.insert(2);
+     ll.insert(3);
+     ll.insert(4);
+     ll.insert(5);
+      ll.insert(6);
+     ll.print();
+     System.out.println(" ");
+     ll.middle();
+  
+     
+    }
+}
