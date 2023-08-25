b70d1a26cbe23eaac11b8524fe4e8fdf12a542ff
Ritika Agarwal <106179540+RitikaAga@users.noreply.github.com>
2023-08-25 18:42:22 +0530

    Create AlternatingBits.java

---
+++ Bit Manipulation/AlternatingBits.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean hasAlternatingBits(int n) {
+        while(n>0){
+            int a = (n&1);
+            n = n>>1;
+            int b = (n&1);
+            if(a==b){
+                return false;
+            }
+        }
+        return true;
+    }
+}
