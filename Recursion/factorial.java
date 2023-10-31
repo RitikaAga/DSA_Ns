c096cbace386341301482e41400b3bc1c5c5f8c4
Ritika Agarwal <106179540+RitikaAga@users.noreply.github.com>
2023-10-31 16:11:55 +0530

    Create factorial.java

---
+++ Recursion/factorial.java
@@ -0,0 +1,13 @@
+class Recursion1 {
+public static void printFactorial(int n, int fact) {
+if(n == 0) {
+System.out.println(fact);
+return;
+}
+fact *= n;
+printFactorial(n-1, fact);
+}
+public static void main(String args[]) {
+printFactorial(5, 1);
+}
+}
